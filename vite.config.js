3da3042a01acd1c28bd3b07067ebaefe56b553ca
Jefferson Ferreira <emanuelly980280644@gmail.com>
2025-10-15 10:38:44 -0300

    config: Adicionado base ao Vite para corrigir carregamento no GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
 
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// O nome do seu repositório é 'desenho-tecnico'
+const REPO_NAME = 'desenho-tecnico';
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Remove configurações de resolve e optimizeDeps
-})
+  base: `/${REPO_NAME}/`, // Adiciona o subcaminho do repositório ao BASE_URL
+});
